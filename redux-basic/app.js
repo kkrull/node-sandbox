af18d43d26ed466ca7be972fa946da3d09557171
Kyle Krull <kyle.krull@here.com>
2016-09-19 23:04:26 -0500

    Doing something to affect the state

--- redux-basic/app.js
+++ redux-basic/app.js
@@ -1,11 +1,20 @@
 const Redux = require('redux');
 
 function counter(state, action) {
-  return state;
+  switch(action.type) {
+    case 'DEC':
+      return state - 1;
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
 }
 
 const store = Redux.createStore(counter, 0);
 store.subscribe(() => console.log(`Count: ${store.getState()}`));
 
 store.dispatch({ type: 'NOP' });
+store.dispatch({ type: 'INC' });
+store.dispatch({ type: 'DEC' });
 

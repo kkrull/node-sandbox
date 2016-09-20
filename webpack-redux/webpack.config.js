77bf343d942a20411447e8a2792a247fbc85c98d
Kyle Krull <kyle.krull@here.com>
2016-09-20 08:38:23 -0500

    Basic webpack build without any loaders

---
+++ webpack-redux/webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+module.exports = {
+  entry: {
+    app: ['./app/main.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    //loaders: [{
+    //test: /\.js$/,
+    //exclude: /node_modules/,
+    //loader: 'babel-loader'
+    //}]
+  }
+};

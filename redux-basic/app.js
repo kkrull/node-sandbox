4e2646352deeb584edb4bff03536a85653dbfd9b
Kyle Krull <kyle.krull@here.com>
2016-09-19 22:49:29 -0500

    NOP state machine.  BOO YA

---
+++ redux-basic/app.js
@@ -0,0 +1,11 @@
+const Redux = require('redux');
+
+function counter(state, action) {
+  return state;
+}
+
+const store = Redux.createStore(counter, 0);
+store.subscribe(() => console.log('Count: ' + store.getState()));
+
+store.dispatch({ type: 'NOP' });
+

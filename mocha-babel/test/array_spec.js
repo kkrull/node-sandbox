db93a25d75b40589ac69e34bf69c84086605ff42
Kyle Krull <kyle.krull@here.com>
2016-09-24 10:06:17 -0500

    Always requiring expect.js [globally](http://stackoverflow.com/questions/31282839/how-to-properly-require-modules-from-mocha-opts-file)

---
+++ mocha-babel/test/array_spec.js
@@ -0,0 +1,13 @@
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      var actual = [1,2,3].indexOf(4);
+      expect(actual).toBe(-1);
+    });
+    it('should return the index when the value is present', function() {
+      var actual = [1,2,3].indexOf(2);
+      expect(actual).toBe(1);
+    });
+  });
+});
+

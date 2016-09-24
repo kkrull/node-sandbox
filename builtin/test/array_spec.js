568898356bb19dc85cfdc09a80a9035423539153
Kyle Krull <kyle.krull@here.com>
2016-09-23 19:47:18 -0500

    Using [expect](https://github.com/mjackson/expect)

--- builtin/test/array_spec.js
+++ builtin/test/array_spec.js
@@ -1,11 +1,14 @@
-var assert = require('assert');
+var expect = require('expect');
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+      var actual = [1,2,3].indexOf(4);
+      expect(actual).toBe(-1);
     });
     it('should return the index when the value is present', function() {
-      assert.equal(1, [1,2,3].indexOf(2));
+      var actual = [1,2,3].indexOf(2);
+      expect(actual).toBe(1);
     });
   });
 });

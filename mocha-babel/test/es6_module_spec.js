4776601f6ead74619ca19493af6d5732896a00ce
Kyle Krull <kyle.krull@here.com>
2016-09-24 11:41:38 -0500

    Works, but which of these changes did the trick? Ran it this way ./node_modules/.bin/mocha --compilers js:babel-register  test/es6_module_spec.js

---
+++ mocha-babel/test/es6_module_spec.js
@@ -0,0 +1,9 @@
+//import { foo } from "../src/es6_module";
+//import expect from 'expect';
+
+require('babel-register')();
+console.log(require.main);
+import expect from 'expect';
+
+describe('ES6 Module', function() {
+});

fa618bd77e6fea3387aa82363bc3da724e01d017
Kyle Krull <kyle.krull@here.com>
2016-09-24 10:22:50 -0500

    Most basic separation of test/production code

--- mocha-babel/test/helper.js
+++ mocha-babel/test/helper.js
@@ -1,2 +1,5 @@
+//I think this will become available even in the production modules.
+//If that's true, then mocha's functions like beforeEach are also available there.
+//While that's not ideal, it's at least not inconsistent.  Use sparingly.
 global.expect = require('expect');
 
---
+++ mocha-babel/test/widget_spec.js
@@ -0,0 +1,8 @@
+const Widget = require('../src/widget');
+
+describe('widget', function() {
+  it('exists', function() {
+    expect(Widget).toExist();
+  });
+});
+

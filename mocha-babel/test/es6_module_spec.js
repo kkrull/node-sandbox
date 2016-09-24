c52c29a966bce9993430dbba380f68e297d9b167
Kyle Krull <kyle.krull@here.com>
2016-09-24 11:45:07 -0500

    Use require statement ./node_modules/.bin/mocha --require babel-register test/es6_module_spec.js

--- mocha-babel/test/es6_module_spec.js
+++ mocha-babel/test/es6_module_spec.js
@@ -1,9 +1,11 @@
 //import { foo } from "../src/es6_module";
 //import expect from 'expect';
 
-require('babel-register')();
-console.log(require.main);
+//require('babel-register')();
 import expect from 'expect';
 
 describe('ES6 Module', function() {
+  it('can call expect', function() {
+    expect({}).toExist();
+  });
 });

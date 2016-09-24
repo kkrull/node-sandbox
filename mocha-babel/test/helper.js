4a501ef8790341b6b633944dfb511092e842a862
Kyle Krull <kyle.krull@here.com>
2016-09-24 11:34:47 -0500

    Trying to use babel-register, but no luck yet

--- mocha-babel/test/helper.js
+++ mocha-babel/test/helper.js
@@ -1,5 +1,7 @@
 //I think this will become available even in the production modules.
 //If that's true, then mocha's functions like beforeEach are also available there.
 //While that's not ideal, it's at least not inconsistent.  Use sparingly.
-global.expect = require('expect');
+
+//global.expect = require('expect');
+//require('babel-register')();
 

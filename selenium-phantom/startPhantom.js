2a3d8353cda1cf657c463c71e31c18a17084cec8
Kyle Krull <kyle.krull@here.com>
2016-10-07 09:30:50 -0500

    Apparently requiring the driver explicitly isn't necessary

--- selenium-phantom/startPhantom.js
+++ selenium-phantom/startPhantom.js
@@ -1,4 +1,4 @@
 var webdriver = require('selenium-webdriver');
-var phantomdriver = require('selenium-webdriver/phantomjs');
+//var phantomdriver = require('selenium-webdriver/phantomjs');
 var driver = new webdriver.Builder().forBrowser('phantomjs').build();
 

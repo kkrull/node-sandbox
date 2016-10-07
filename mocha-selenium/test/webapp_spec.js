9cb5e1fd4984fff175c4982a3636097d2cb37735
Kyle Krull <kyle.krull@here.com>
2016-10-07 17:19:56 -0500

    Gearing up to start phantom with regular selenium-webdriver

--- mocha-selenium/test/webapp_spec.js
+++ mocha-selenium/test/webapp_spec.js
@@ -13,16 +13,28 @@ describe('web application', function() {
     });
   });
 
-  it('responds to GET /', function(done) {
-    request(this.app).get('/')
-      .expect(200, done);
-  });
-  it('responds with text in the message body', function(done) {
-    request(this.app).get('/')
-      .expect(function(res) {
-        expect(res.text).toEqual('Hello World!');
-      })
-      .expect(200, done);
+  context('testing HTTP response with supertest', function() {
+    it('responds to GET /', function(done) {
+      request(this.app).get('/')
+        .expect(200, done);
+    });
+    it('responds with text in the message body', function(done) {
+      request(this.app).get('/')
+        .expect(function(res) {
+          expect(res.text).toEqual('Hello World!');
+        })
+        .expect(200, done);
+    });
+  }); 
+
+  context('testing rendering with selenium -> webdriver -> ghostdriver -> phantom', function() {
+    it('loads phantomjs', function(done) {
+      var webdriver = require('selenium-webdriver');
+      //var phantomdriver = require('selenium-webdriver/phantomjs');
+      var driver = new webdriver.Builder().forBrowser('phantomjs').build();
+      done('pending'); //TODO KDK - how to know when the browser is running?
+      //There's a nice example for later with [webdriverio](https://www.npmjs.com/package/phantomjs-prebuilt)
+    });
   });
 });
 

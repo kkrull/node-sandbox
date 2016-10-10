c0364588beb483ad92f2c1633ec809ddfca7f9da
Kyle Krull <kyle.krull@here.com>
2016-10-10 09:22:51 -0500

    WebDriver#get appears to be calling the success callback even when it can't possibly be connecting to what I told it to I'd prefer to build up from the low-level libraries to understand what is being automated, but this is crazy. Try webdriver.io instead

--- mocha-selenium/test/webapp_spec.js
+++ mocha-selenium/test/webapp_spec.js
@@ -13,7 +13,7 @@ describe('web application', function() {
     });
   });
 
-  context('testing HTTP response with supertest', function() {
+  context.skip('testing HTTP response with supertest', function() {
     it('responds to GET /', function(done) {
       request(this.app).get('/')
         .expect(200, done);
@@ -29,11 +29,22 @@ describe('web application', function() {
 
   context('testing rendering with selenium -> webdriver -> ghostdriver -> phantom', function() {
     it('loads phantomjs', function(done) {
-      var webdriver = require('selenium-webdriver');
-      //var phantomdriver = require('selenium-webdriver/phantomjs');
-      var driver = new webdriver.Builder().forBrowser('phantomjs').build();
-      done('pending'); //TODO KDK - how to know when the browser is running?
+      this.timeout(5000);
+
       //There's a nice example for later with [webdriverio](https://www.npmjs.com/package/phantomjs-prebuilt)
+      var webdriver = require('selenium-webdriver');
+      var driver = new webdriver.Builder().forBrowser('phantomjs').build();
+      var requestUrl = 'http://www.google.com'; //this.app.address;
+      console.log(`making request: ${requestUrl}`);
+      driver
+        .get(requestUrl)
+        .then(
+          function(args) { console.log(`[then ok handler] args: ${JSON.stringify(args)}`); }, 
+          function() { console.log('[then error handler]'); })
+        .then(function() {
+          console.log('[then] calling done');
+          done();
+        });
     });
   });
 });

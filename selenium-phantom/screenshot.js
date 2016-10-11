69d4a15bcaefa72460ac3fd48a743593ed9571e3
Kyle Krull <kyle.krull@here.com>
2016-10-11 08:49:13 -0500

    Refactoring to use more of their non-standard Promise API

--- selenium-phantom/screenshot.js
+++ selenium-phantom/screenshot.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const fs = require('fs');
-const webdriver = require('selenium-webdriver');
+const webDriver = require('selenium-webdriver');
 
 function makeApp() {
   const app = express();
@@ -14,21 +14,26 @@ function makeApp() {
   return app;
 }
 
-function writeScreenShot(data, name) {
-  name = name || 'ss.png';
-  fs.writeFileSync(name, data, 'base64');
-}
-
-let app = makeApp();
-let server = app.listen(3000);
-let browser = new webdriver.Builder()
+let server = makeApp().listen(3000);
+let browser = new webDriver.Builder()
   .usingServer()
   .withCapabilities({'browserName': 'phantomjs' })
   .build();
 
-browser.get('http://localhost:3000');
-browser.takeScreenshot().then(function(data) {
-  writeScreenShot(data, 'out1.png');
+console.log('1 - OPENING PAGE');
+browser.get('http://localhost:3000').then(function() {
+  console.log('2 - TAKING SCREEN SHOT');
+  browser.takeScreenshot();
+})
+.then(function(data) {
+  console.log('3 - SAVING SCREEN SHOT');
+  fs.writeFileSync('out1.png', data, 'base64');
+})
+.then(function() {
+  console.log('4 - SHUTTING DOWN BROWSER');
   browser.quit();
+})
+.then(function() {
+  console.log('5 - SHUTTING DOWN SERVER');
   server.close();
 });

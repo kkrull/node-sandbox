2e61ca8eb8e505e0f50d580782db31f69c6e49f1
Kyle Krull <kyle.krull@here.com>
2016-10-11 08:36:45 -0500

    Using a locally-run server instead of an external webpage

--- selenium-phantom/screenshot.js
+++ selenium-phantom/screenshot.js
@@ -1,27 +1,34 @@
 "use strict";
 
-var fs = require('fs');
-var webdriver = require('selenium-webdriver');
-var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'phantomjs' }).build();
+const express = require('express');
+const fs = require('fs');
+const webdriver = require('selenium-webdriver');
 
-browser.manage().window().setSize(1280, 720);
-browser.manage().window().getSize().then(
-  function(size) {
-    console.log("Browser size: " + JSON.stringify(size));
+function makeApp() {
+  const app = express();
+  app.get('/', function(req, res) {
+    res.status(200)
+      .send('Hello world');
   });
-browser.get('http://en.wikipedia.org/wiki/Wiki');
 
-function writeScreenshot(data, name) {
+  return app;
+}
+
+function writeScreenShot(data, name) {
   name = name || 'ss.png';
   fs.writeFileSync(name, data, 'base64');
-};
+}
 
+let app = makeApp();
+let server = app.listen(3000);
+let browser = new webdriver.Builder()
+  .usingServer()
+  .withCapabilities({'browserName': 'phantomjs' })
+  .build();
+
+browser.get('http://localhost:3000');
 browser.takeScreenshot().then(function(data) {
-  writeScreenshot(data, 'out1.png');
-});
-
-browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then(function(links){
-  console.log('Found', links.length, 'Wiki links.' )
+  writeScreenShot(data, 'out1.png');
   browser.quit();
+  server.close();
 });
-

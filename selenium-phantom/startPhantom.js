28c4f1b21059fce13c9b4023a2fccaa889b7e5d3
Kyle Krull <kyle.krull@here.com>
2016-10-11 08:50:16 -0500

    Don't need the other files anymore

--- selenium-phantom/startPhantom.js
+++
@@ -1,4 +0,0 @@
-var webdriver = require('selenium-webdriver');
-//var phantomdriver = require('selenium-webdriver/phantomjs');
-var driver = new webdriver.Builder().forBrowser('phantomjs').build();
-

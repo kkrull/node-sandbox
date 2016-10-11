f7f6c24c4a4f2335c74ef837a275443eaa9393b8
Kyle Krull <kyle.krull@here.com>
2016-10-11 13:55:08 -0500

    Working spec for connecting an in-process Express server to a headless webkit in an external process

--- mocha-selenium-phantom/test/phantom_spec.js
+++ mocha-selenium-phantom/test/phantom_spec.js
@@ -2,31 +2,55 @@ const expect = require('expect');
 const express = require('express');
 const fs = require('fs');
 const webDriver = require('selenium-webdriver');
+const By = webDriver.By;
 
 describe('Example application', function() {
+  function greeterApp() {
+    const app = express();
+    app.get('/', function(req, res) {
+      res.status(200);
+      res.format({
+        html: function() {
+          res.send('<div>Hello world</div>');
+        }
+      });
+    });
+
+    return app;
+  }
+
   beforeEach(function() {
-    expect('server').toBe('RUNNING');
+    this.timeout(15000);
+    console.log('STARTING EXPRESS');
+    this.server = greeterApp().listen(3000);
   });
 
-  it('returns text', function(done) {
-    this.timeout(15000);
+  afterEach(function() {
+    this.server.close();
+  });
+
+  it('greets the user', function(done) {
     let browser = new webDriver.Builder()
       .usingServer()
       .withCapabilities({ 'browserName': 'phantomjs' })
       .build();
 
     console.log('OPENING PAGE');
-    browser.get('http://www.google.com').then(function() {
-      console.log('GETTING TITLE');
-      return browser.getTitle();
-    })
-    .then(function(title) {
-      console.log(`EVALUATING TITLE: ${title}`);
-      expect(title).toEqual('Google');
-    })
-    .then(function() {
-      console.log('TEST DONE');
-      done();
-    });
+    browser.get('http://localhost:3000');
+
+    console.log('GETTING CONTENT');
+    browser.findElement(By.css('div'))
+      .then(function(element) {
+        console.log('GETTING TEXT');
+        return element.getText();
+      })
+      .then(function(text) {
+        console.log(`EVALUATING CONTENT: ${text}`);
+        expect(text).toEqual('Hello world');
+      })
+      .then(function() {
+        console.log('TEST DONE');
+        done();
+      });
   });
 });

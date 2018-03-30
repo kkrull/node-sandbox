5995cf8ac5558f78b2dc59e487907c20e7cc3c3c
Kyle Krull <kkrull@8thlight.com>
2018-03-30 16:08:38 -0500

    Loading audio as an arraybuffer

--- webaudio/public/main.js
+++ webaudio/public/main.js
@@ -1,30 +1,41 @@
+let AudioContext;
+let superagent;
+
 function main() {
-  return doMain(window);
+  AudioContext = window.AudioContext || window.webkitAudioContext;
+  superagent = window.superagent;
+  return doMain();
 }
 
-function doMain(window) {
+function doMain() {
   Promise.all([
-    initAudioContext(window),
-    fetchAudioBuffer('audio/sf2-prepare-yourself.wav')
-  ]).then(([context, buffer]) => playAudio(buffer, context));
+    initAudioContext(),
+    fetchArrayBuffer('audio/sf2-prepare-yourself.wav')
+  ]).then(([context, buffer]) => playAudio(buffer, context))
+    .catch(error => console.log('Failed to load or play audio', error));
 }
 
-function initAudioContext(window) {
+function initAudioContext() {
   try {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
     return Promise.resolve(new AudioContext());
   } catch(e) {
     return Promise.reject('Web Audio API is not supported in this browser');
   }
 }
 
-function fetchAudioBuffer(url) {
-  return Promise.resolve('BUFFER');
+function fetchArrayBuffer(url) {
+  return new Promise((resolve, reject) => {
+    superagent.get(url)
+      .responseType('arraybuffer')
+      .end((error, response) => error && reject(error) || resolve(response))
+  });
 }
 
 function playAudio(buffer, context) {
   console.log('context', context);
   console.log('buffer', buffer);
+  // context.decodeAudioData(request.response, function(buffer) {
+  //   dogBarkingBuffer = buffer;
 }
 
 window.addEventListener('load', main, false);

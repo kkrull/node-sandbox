5c774a4ebc852902d07889d21b8641a27272a4be
Kyle Krull <kkrull@8thlight.com>
2018-04-06 16:34:33 -0500

    Cleanup

--- electron_webaudio/main.js
+++ electron_webaudio/main.js
@@ -3,10 +3,7 @@ const path = require('path')
 const url = require('url')
 
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
-
-  // and load the index.html of the app.
+  const win = new BrowserWindow({ width: 800, height: 600 })
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',

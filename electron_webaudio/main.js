b97387ec2bdfe56a96e9e865d10f7beeeb312f4c
Kyle Krull <kkrull@8thlight.com>
2018-04-06 16:56:10 -0500

    Adding platform-appropriate window behavior

--- electron_webaudio/main.js
+++ electron_webaudio/main.js
@@ -2,13 +2,37 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
 
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
 function createWindow() {
-  const win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1280, height: 720 })
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'renderer', 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
+
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    //Allow garbage collection of the window
+    win = null
+  })
 }
 
 app.on('ready', createWindow)
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if(win === null) {
+    createWindow()
+  }
+})
+
+app.on('window-all-closed', () => {
+  if(process.platform !== 'darwin') {
+    app.quit()
+  }
+})

8c5d411f6b44071a36cc3f593f4363ffae48a083
Kyle Krull <kkrull@8thlight.com>
2018-04-20 17:20:56 -0500

    Starting to stub out a prototype UI for listing and playing files

--- electron_webaudio/renderer/renderer-main.js
+++ electron_webaudio/renderer/renderer-main.js
@@ -1,13 +1,34 @@
 const { ipcRenderer } = require('electron')
 
 function main() {
+  listLocalFiles().then(files => renderFileSelectors(files))
+
   const context = new AudioContext()
   loadLocalFile('audio/sf2-new-challenger.wav')
     .then(nodeBuffer => context.decodeAudioData(nodeBuffer.buffer))
     .then(decoded => startPlayback(decoded, context))
-    .then(() => console.log('success'))
     .catch(error => console.log('Failed to load or play audio', error))
 }
+
+/* Listing */
+
+function listLocalFiles() {
+  return Promise.resolve(['one', 'two'])
+}
+
+function renderFileSelectors(files) {
+  const list = document.getElementsByClassName('file-list')[0]
+  files.map(file => renderFileSelector(file))
+    .forEach(selector => list.appendChild(selector))
+}
+
+function renderFileSelector(file) {
+  const fileItem = document.createElement('li')
+  fileItem.appendChild(document.createTextNode(file))
+  return fileItem
+}
+
+/* Playback */
 
 function loadLocalFile(filename) {
   return new Promise((resolve, reject) => {

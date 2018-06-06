6a4723af52541e27e589cbec80e8fc6c5b3dfd5f
Kyle Krull <kkrull@8thlight.com>
2018-06-06 15:51:35 -0500

    Angular tutorial: Application Shell

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Tour of Heroes!');
+    expect(compiled.querySelector('h1').textContent).toEqual('Tour of Heroes');
   }));
 });

d5e4e8c1d2a834912edc680f6fe691e236c0106f
Kyle Krull <kkrull@8thlight.com>
2018-06-07 09:53:47 -0500

    Sorted

--- angular-tour-of-heroes/karma.conf.js
+++ angular-tour-of-heroes/karma.conf.js
@@ -1,10 +1,24 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
+    angularCli: {
+      environment: 'dev'
+    },
+    autoWatch: true,
     basePath: '',
+    browsers: ['Chrome'],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    colors: true,
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
     frameworks: ['jasmine', '@angular/cli'],
+    logLevel: config.LOG_INFO,
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -12,22 +26,8 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
+    reporters: ['progress', 'kjhtml'],
     singleRun: false
   });
 };

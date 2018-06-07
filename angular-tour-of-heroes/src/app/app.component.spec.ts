b3398706d798fe413b3fa013e095dbe798952594
Kyle Krull <kkrull@8thlight.com>
2018-06-07 11:13:43 -0500

    Breaking dependencies in tests with shallow rendering

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -1,34 +1,39 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from "./heroes/heroes.component";
-import { FormsModule } from "@angular/forms";
+import { Component } from "@angular/core";
+import { By } from "@angular/platform-browser";
+
+@Component({ selector: 'app-heroes', template: '<div id="heroes-component"></div>' })
+class HeroesComponentStub {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent, HeroesComponent
+        AppComponent,
+        HeroesComponentStub
       ],
-      imports: [FormsModule]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  describe('.title', () => {
+    it('the property value is "Tour of Heroes"', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.title).toEqual('Tour of Heroes');
+    });
 
-  it('should have a suitable title', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Tour of Heroes');
-  }));
+    it('renders in the header element', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const headerElement = fixture.nativeElement.querySelector('h1');
+      expect(headerElement.textContent).toEqual('Tour of Heroes');
+    });
+  });
 
-  it('should render the in a header tag', async(() => {
+  it('renders a HeroesComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toEqual('Tour of Heroes');
-  }));
+    expect(fixture.debugElement.query(By.directive(HeroesComponentStub))).not.toBeNull('HeroesComponent');
+  });
 });

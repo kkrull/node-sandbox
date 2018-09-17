365ecdc63d68f926c5647d328b04528cc0265456
Kyle Krull <kkrull@8thlight.com>
2018-09-17 18:33:53 -0500

    Command line interface for node-http

--- node-http/node_http.js
+++ node-http/node_http.js
@@ -1,8 +1,25 @@
-var http = require('http');
+const http = require('http');
+const process = require('process');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(8080);
+function main(args) {
+  const { host, port } = parseArgs(args);
+  const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello Http');
+  });
 
+  console.log(`Listening on ${host}:${port}`);
+  server.listen(port);
+}
+
+function parseArgs(argv) {
+  if(argv.length !== 4)
+    throw new Error(`Usage: ${argv[0]} ${argv[1]} <host> <port>`);
+
+  return {
+    host: argv[2],
+    port: Number(argv[3])
+  };
+}
+
+main(process.argv);

719164333765ff52ffe4fcf9ec7e098e0a243701
Kyle Krull <kkrull@8thlight.com>
2018-09-17 18:48:56 -0500

    Add request loggin

--- node-http/node_http.js
+++ node-http/node_http.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const process = require('process');
+const url = require('url');
 
 function main(args) {
   const { host, port } = parseArgs(args);
-  const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('Hello Http');
+  const server = http.createServer((request, response) => {
+    console.log();
+    logRequest(request);
+    handleRequest(request, response);
   });
 
   console.log(`Listening on ${host}:${port}`);
@@ -20,6 +22,29 @@ function parseArgs(argv) {
     host: argv[2],
     port: Number(argv[3])
   };
+}
+
+function logRequest(request) {
+  console.log(`> ${request.method} ${request.url}`);
+  for(var i = 0; i < request.rawHeaders.length; i += 2) {
+    console.log(`  ${request.rawHeaders[i]}: ${request.rawHeaders[i+1]}`);
+  }
+}
+
+function handleRequest(request, response) {
+  const requestUrl = url.parse(request.url);
+  switch(requestUrl.pathname) {
+    case '/headers':
+      response.writeHead(200);
+      for(var i = 0; i < request.rawHeaders.length; i += 2) {
+        response.write(`${request.rawHeaders[i]}: ${request.rawHeaders[i+1]}\n`);
+      }
+
+      response.end();
+    default:
+      response.writeHead(200);
+      response.end('Hello Http');
+  }
 }
 
 main(process.argv);

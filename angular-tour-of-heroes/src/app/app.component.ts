beeddf36ee1a2004082c1ba18918fb763aeb11a8
Kyle Krull <kkrull@8thlight.com>
2019-01-14 10:35:54 -0600

    Add router
    https://v5.angular.io/guide/router

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Angular Router</h1>
+    <nav>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
-}
+export class AppComponent { }
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './hero-list.component';
+import { PageNotFoundComponent } from './not-found.component';
+
 import { HeroesComponent } from './heroes/heroes.component';
 
+const appRoutes: Routes = [
+  { path: 'heroes', component: HeroListComponent },
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroListComponent,
+    HeroesComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }

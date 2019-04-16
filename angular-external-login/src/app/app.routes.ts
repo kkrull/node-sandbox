384884e6ecb2f035c7f063f9d6b98c8870281e56
Kyle Krull <kkrull@8thlight.com>
2019-04-16 11:16:10 -0500

    Redirect to Cognito user pool

--- angular-external-login/src/app/app.routes.ts
+++ angular-external-login/src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { ExternalUrlResolverToken } from './login/services/tokens';
 import { TutorialComponent } from './tutorial/tutorial.component';
 
 const loginRouteData: ExternalUrlRouteData = {
-  targetUrl: new URL('http://www.google.com')
+  targetUrl: new URL('https://skydevelopers.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=2fe9dfh5ictve3n26c3jjvph2l&redirect_uri=https://oidcdebugger.com/debug')
 };
 
 export const APP_ROUTES: Routes = [

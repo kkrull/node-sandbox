2aa0b90f035990316a0317d4611feac906aa652c
Kyle Krull <kkrull@8thlight.com>
2019-04-16 11:54:24 -0500

    Parse URL fragment

--- angular-external-login/src/app/login/login-callback.component.ts
+++ angular-external-login/src/app/login/login-callback.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { map, tap } from 'rxjs/operators';
+
+interface Tokens {
+  accessToken: string;
+  expiresIn: string;
+  idToken: string;
+  type: string;
+}
 
 @Component({
   selector: 'app-login-callback',
   templateUrl: './login-callback.component.html',
   styleUrls: ['./login-callback.component.css']
 })
-export class LoginCallbackComponent { }
+export class LoginCallbackComponent {
+  urlFragment$: Observable<string>;
+  tokens$: Observable<Tokens>;
+
+  constructor(private route: ActivatedRoute) {
+    this.urlFragment$ = route.fragment;
+
+    this.tokens$ = route.fragment.pipe(
+      map(fragment => {
+        const params = new URLSearchParams(fragment);
+        return {
+          accessToken: params.get('access_token'),
+          expiresIn: params.get('expires_in'),
+          idToken: params.get('id_token'),
+          type: params.get('token_type'),
+        };
+      })
+    );
+  }
+}

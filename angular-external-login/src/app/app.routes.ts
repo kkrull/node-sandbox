6664821247d738b0899e94ee2eb37d0556364eff
Kyle Krull <kkrull@8thlight.com>
2019-04-16 11:21:36 -0500

    Redirect back to some part of the app, after signing in

--- angular-external-login/src/app/app.routes.ts
+++ angular-external-login/src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { ExternalUrlResolverToken } from './login/services/tokens';
 import { TutorialComponent } from './tutorial/tutorial.component';
 
 const loginRouteData: ExternalUrlRouteData = {
-  targetUrl: new URL('https://skydevelopers.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=2fe9dfh5ictve3n26c3jjvph2l&redirect_uri=https://oidcdebugger.com/debug')
+  targetUrl: new URL('https://skydevelopers.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=2fe9dfh5ictve3n26c3jjvph2l&redirect_uri=http://localhost:4200/tutorial')
 };
 
 export const APP_ROUTES: Routes = [

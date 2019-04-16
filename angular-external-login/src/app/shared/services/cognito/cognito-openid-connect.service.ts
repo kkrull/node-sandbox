5707a9b29b154eb96ad13322472bb4f5a14a49f7
Kyle Krull <kkrull@8thlight.com>
2019-04-16 16:22:13 -0500

    Refactor to prepare for querying discovery document

--- angular-external-login/src/app/shared/services/cognito/cognito-openid-connect.service.ts
+++ angular-external-login/src/app/shared/services/cognito/cognito-openid-connect.service.ts
@@ -1,15 +1,54 @@
 import { OpenIdConnectService } from '../../../login/services/change-to-sign-in-url.resolve';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { map, tap } from 'rxjs/operators';
+
+interface ImplicitAppClientConfig {
+  clientId: string;
+}
+
+interface IdentityProviderConfig {
+  baseUrl: URL;
+  appClient: ImplicitAppClientConfig;
+}
 
 export class CognitoOpenIdConnectService extends OpenIdConnectService {
-  authorizationUrl(): Observable<URL> {
-    const configUrl = new URL('https://cognito-idp.us-east-1.amazonaws.com/us-east-1_N8OfbsdVa/.well-known/openid-configuration');
+  private config: IdentityProviderConfig;
 
-    const authorizationUrl = new URL('https://skydevelopers.auth.us-east-1.amazoncognito.com/oauth2/authorize');
-    authorizationUrl.searchParams.append('client_id', '2fe9dfh5ictve3n26c3jjvph2l');
+  constructor() {
+    super();
+
+    this.config = {
+      baseUrl: new URL('https://cognito-idp.us-east-1.amazonaws.com/us-east-1_N8OfbsdVa'),
+      appClient: {
+        clientId: '2fe9dfh5ictve3n26c3jjvph2l'
+      }
+    };
+  }
+
+  authorizationUrl(): Observable<URL> {
+    return of(this.configUrl()).pipe(
+      map(configUrl => this.authorizationEndpoint(configUrl)),
+      map(authEndpointUrl => this.authorizeThisApp(authEndpointUrl)),
+      tap(x => console.log('authorization url', x.href))
+    );
+  }
+
+  private configUrl(): URL {
+    const configUrl = new URL(this.config.baseUrl.href);
+    configUrl.pathname = configUrl.pathname + '/.well-known/openid-configuration';
+    return configUrl;
+  }
+
+  private authorizationEndpoint(configUrl: URL): URL {
+    return new URL('https://skydevelopers.auth.us-east-1.amazoncognito.com/oauth2/authorize');
+  }
+
+  private authorizeThisApp(authorizationEndpoint: URL): URL {
+    const authorizationUrl = new URL(authorizationEndpoint.href);
+    authorizationUrl.searchParams.append('client_id', this.config.appClient.clientId);
     authorizationUrl.searchParams.append('redirect_uri', 'http://localhost:4200/callback');
     authorizationUrl.searchParams.append('response_type', 'token');
-    return of(authorizationUrl);
+    return authorizationUrl;
   }
 }

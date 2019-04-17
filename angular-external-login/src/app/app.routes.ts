a13addf077130d103f7864c97f948de8734972ec
Kyle Krull <kkrull@8thlight.com>
2019-04-17 14:23:08 -0500

    Move auth/ routes to own file

--- angular-external-login/src/app/app.routes.ts
+++ angular-external-login/src/app/app.routes.ts
@@ -2,35 +2,14 @@ import { Routes } from '@angular/router';
 
 import { AuthGuard } from './lib/auth-guard';
 
-import { LoginComponent } from './auth/login.component';
-import { LoginCallbackComponent } from './auth/login-callback.component';
-import { LogoutComponent } from './auth/logout.component';
-import { ChangeToExternalSignInUrl } from './lib/change-to-sign-in-url.resolve';
+import { AUTH_ROUTES } from './auth/auth.routes';
 import { GuardedComponent } from './guarded/guarded.component';
 import { ReferenceComponent } from './reference/reference.component';
-
-const authRoutes: Routes = [
-  {
-    path: 'callback',
-    component: LoginCallbackComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-    resolve: {
-      _unused: ChangeToExternalSignInUrl
-    }
-  },
-  {
-    path: 'logout',
-    component: LogoutComponent
-  }
-];
 
 export const APP_ROUTES: Routes = [
   {
     path: 'auth',
-    children: authRoutes
+    children: AUTH_ROUTES
   },
   {
     path: 'guarded',
---
+++ angular-external-login/src/app/auth/auth.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+import { ChangeToExternalSignInUrl } from '../lib/change-to-sign-in-url.resolve';
+
+import { LoginComponent } from './login.component';
+import { LoginCallbackComponent } from './login-callback.component';
+import { LogoutComponent } from './logout.component';
+
+export const AUTH_ROUTES: Routes = [
+  {
+    path: 'callback',
+    component: LoginCallbackComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    resolve: {
+      _unused: ChangeToExternalSignInUrl
+    }
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  }
+];

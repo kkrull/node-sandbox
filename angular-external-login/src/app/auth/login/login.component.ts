9bf728d1d5e3500497ebf8cb4209b7fbcb00461b
Kyle Krull <kkrull@8thlight.com>
2019-04-18 10:08:18 -0500

    Clarify intent of a single-use subscription
    that automatically unsubscribes / disposes

--- angular-external-login/src/app/auth/login/login.component.ts
+++ angular-external-login/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { take, tap } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 
 import { OpenIdConnectService } from '../../shared/services/interfaces/openid-connect.service';
 
@@ -17,9 +17,12 @@ export class LoginComponent implements OnInit {
               private navigationService: NavigationService) { }
 
   ngOnInit(): void {
-    this.identityProviderService.authorizationUrl().pipe(
-      take(1),
-      tap(targetUrl => this.navigationService.changeLocationTo(targetUrl))
-    ).subscribe();
+    this.authorizationUrlSnapshot()
+      .subscribe(url => this.navigationService.changeLocationTo(url));
+  }
+
+  private authorizationUrlSnapshot() {
+    return this.identityProviderService.authorizationUrl()
+      .pipe(take(1));
   }
 }

ab59db7156689f2cbc914dd9cfce3ee6d7c0bb47
Kyle Krull <kkrull@8thlight.com>
2019-04-18 14:59:04 -0500

    No need to import the child routes in AuthModule

--- angular-external-login/src/app/auth/auth.module.ts
+++ angular-external-login/src/app/auth/auth.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AUTH_ROUTES } from './auth.routes';
 import { CallbackComponent } from './login/callback.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { NavigationService, WindowNavigationService } from './window-navigation.service';
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(AUTH_ROUTES)
-  ],
   declarations: [
     CallbackComponent,
     LoginComponent,

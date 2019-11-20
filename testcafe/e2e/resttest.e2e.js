7e013194bcc1d071ab6203b648aad9a980c651ff
Kyle Krull <kkrull@8thlight.com>
2019-11-19 20:24:57 -0600

    Introduce test metadata for filtering tests

--- testcafe/e2e/resttest.e2e.js
+++ testcafe/e2e/resttest.e2e.js
@@ -9,18 +9,19 @@ fixture('REST Test')
   .page('https://resttesttest.com/')
   .requestHooks(requestHook);
 
-test('Makes an async request', async t => {
-  const urlInput = Selector('#urlvalue');
-  const submitButton = Selector('#submitajax');
-  await t
-    .typeText(urlInput, 'https://httpbin.org/get', { replace: true })
-    .click(submitButton);
+test.meta({ wip: 'true' })
+  ('Makes an async request', async t => {
+    const urlInput = Selector('#urlvalue');
+    const submitButton = Selector('#submitajax');
+    await t
+      .typeText(urlInput, 'https://httpbin.org/get', { replace: true })
+      .click(submitButton);
 
-  const responseStatus = Selector('#statuspre');
-  await t
-    .expect(responseStatus.textContent).contains('HTTP')
-    .expect(responseStatus.textContent).contains('200 OK');
+    const responseStatus = Selector('#statuspre');
+    await t
+      .expect(responseStatus.textContent).contains('HTTP')
+      .expect(responseStatus.textContent).contains('200 OK');
 
-  const statusText = await responseStatus.textContent;
-  console.log('Final status', statusText);
-});
+    const statusText = await responseStatus.textContent;
+    console.log('Final status', statusText);
+  });

d04a6a83307b567a9602ce0f51a93267a064bf28
Kyle Krull <kkrull@8thlight.com>
2021-07-07 16:55:31 -0500

    Format with terraform fmt
    Ew.

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
@@ -11,5 +11,5 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region = "us-east-1"
+  region  = "us-east-1"
 }

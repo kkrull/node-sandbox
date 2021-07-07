f327a7df5b789126a54fb8d761b14c6a8a568f5e
Kyle Krull <kkrull@8thlight.com>
2021-07-07 14:59:27 -0500

    Create a docker container from a known image

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -6,3 +6,19 @@ terraform {
     }
   }
 }
+
+provider "docker" { }
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}

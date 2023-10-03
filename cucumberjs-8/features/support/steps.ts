5f9191c7136cfa588afd6bc87b5245e846f9431f
Kyle Krull <kdkrull@gmail.com>
2023-10-03 02:03:56 -0500

    next step

--- cucumberjs-8/features/support/steps.ts
+++ cucumberjs-8/features/support/steps.ts
@@ -2,6 +2,7 @@ import assert from "assert";
 import { When, Then } from "@cucumber/cucumber";
 import { Greeter } from "../../src/index";
 
+//TODO KDK: clean this up
 When("the greeter says hello", function () {
   const subject = new Greeter();
   this.whatIHeard = subject.sayHello();

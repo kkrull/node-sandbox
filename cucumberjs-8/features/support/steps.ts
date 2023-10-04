30f49faada848ee088a3f4e3b3b3a109b7d2411b
Kyle Krull <kdkrull@gmail.com>
2023-10-04 00:13:31 -0500

    Use arrow functions for step definitions

--- cucumberjs-8/features/support/steps.ts
+++ cucumberjs-8/features/support/steps.ts
@@ -2,12 +2,13 @@ import assert from 'assert';
 import { When, Then } from '@cucumber/cucumber';
 import { Greeter } from '../../src/index';
 
-//TODO KDK: clean this up
-When('the greeter says hello', function () {
+let whatIHeard: string;
+
+When('the greeter says hello', () => {
   const subject = new Greeter();
-  this.whatIHeard = subject.sayHello();
+  whatIHeard = subject.sayHello();
 });
 
-Then('I should have heard {string}', function (expected) {
-  assert.equal(expected, this.whatIHeard);
+Then('I should have heard "Hello"', () => {
+  assert.equal('Hello', whatIHeard);
 });
